8cdbe652d7b4c2f2b34af4cec841c3747431f184
SamerHamish <e.samerhamish@gmail.com>
2026-04-21 15:33:32 +0200

    Add domain classes.

---
+++ Domain/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public required string CustomerName { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+    }
+}
---
+++ Domain/OrderItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class OrderItem
+    {
+        public Guid Id { get; set; }
+        public required string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
