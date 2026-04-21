60504aa7c8d7bd5798a6b8a5f409928d10f429b3
SamerHamish <e.samerhamish@gmail.com>
2026-04-21 16:27:51 +0200

    Create DbContext in Infrastucture project.

---
+++ Infrastructure/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
